dda30ddd405786f6f41c4b5ef89bca2dd246aef1
laurenfazah <laurenfazah@gmail.com>
2017-06-01 15:46:35 -0600

    Made Reservations#create uglier for more refactor focus

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,7 +10,11 @@ class ReservationsController < ProtectedController
       if @reservation.save
         @reservation.room.booked!
         redirect_to guest_reservations_path(current_guest)
+      else
+        redirect_to hotel_room_path(@reservation.room.hotel, @reservation.room)
       end
+    else
+      redirect_to hotel_room_path(@reservation.room.hotel, @reservation.room)
     end
   end
 
