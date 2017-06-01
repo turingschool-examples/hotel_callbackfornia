1c3623ed4a75bd8bb9e37c791d8e33650a7a5ac3
laurenfazah <laurenfazah@gmail.com>
2017-06-01 09:58:26 -0600

    Seeds ready to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create hotels
+10.times do
+  hotel = Hotel.create(
+    name: Faker::Company.name,
+    location: Faker::Address.state
+  )
+  puts "Hotel #{hotel.name} located in #{hotel.location} created."
+end
+
+
+# create rooms for hotels
+Hotel.all.each do |hotel|
+  10.times do
+    room = Room.create(
+      number: Faker::Address.building_number,
+      hotel_id: hotel.id
+    )
+    puts "Room number #{room.number} for Hotel #{hotel.name} created."
+  end
+end
+
+# create guests
+50.times do
+  guest = Guest.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    credit_card_number: Faker::Business.credit_card_number
+  )
+  puts "Guest #{guest.first_name} #{guest.last_name} created."
+end
+
+# create reservations
+50.times do
+  guest_ids = Guest.pluck(:id)
+  room_ids = Room.pluck(:id)
+
+  guest = Guest.find(guest_ids.sample)
+  room = Room.find(room_ids.sample)
+
+  rez = Reservation.create(
+    room_id: room.id,
+    guest_id: guest.id
+  )
+  puts "Reservation #{rez.id} created for #{guest.first_name} #{guest.last_name} in room #{room.number}."
+end
